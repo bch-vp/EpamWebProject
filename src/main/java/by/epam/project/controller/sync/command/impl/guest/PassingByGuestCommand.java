548ca3d61363e66dc22113e3c0f14c02cb333878
bch-vp <ilya.murin.work@gmail.com>
2021-02-09 18:45:42 +0300

    R

--- src/main/java/by/epam/project/controller/sync/command/impl/guest/PassingByGuestCommand.java
+++ src/main/java/by/epam/project/controller/sync/command/impl/guest/PassingByGuestCommand.java
@@ -15,6 +15,8 @@ public class PassingByGuestCommand implements Command {
     @Override
     public Router execute(HttpServletRequest request) {
         logger.info("pass guest");
+        logger.warn("fgawgf");
+        logger.fatal("fwagfa");
         return new Router(PagePath.PASSING_BY_GUEST);
     }
 }
