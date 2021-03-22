da5fba3cb9f732fc3509c43f2eaa0eded0f4e3c7
bch-vp <ilya.murin.work@gmail.com>
2021-03-22 22:52:25 +0300

    Create logic on error 403

--- src/main/java/by/epam/project/controller/parameter/PagePath.java
+++ src/main/java/by/epam/project/controller/parameter/PagePath.java
@@ -6,10 +6,11 @@ public class PagePath {
     public final static String PASSING_BY_CLIENT = "/WEB-INF/jsp/role/client.jsp";
     public final static String PASSING_BY_ADMIN = "/WEB-INF/jsp/role/admin.jsp";
 
-    public final static String ERROR_404 = "/WEB-INF/jsp/error404.jsp";
-    public final static String ERROR_500 = "/WEB-INF/jsp/error500.jsp";
+    public final static String ERROR_403 = "/jsp/error403.jsp";
+    public final static String ERROR_404 = "/jsp/error404.jsp";
+    public final static String ERROR_500 = "/jsp/error500.jsp";
 
-    public final static String NOTIFICATION_SUCCESS = "/WEB-INF/jsp/notificationSuccess.jsp";
+    public final static String NOTIFICATION_SUCCESS = "/jsp/notificationSuccess.jsp";
 
     private PagePath() {
     }
